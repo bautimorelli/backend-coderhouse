e33bd64793e144b982bd9cdfe521f89b4ec00636
Bautista Morelli <bautistamorelli@gmail.com>
2022-11-10 16:36:27 -0300

    Entrega 5 - Pug & EJS

--- Entrega 5/Pug/src/server.js
+++ Entrega 5/Pug/src/server.js
@@ -1,39 +1,36 @@
 const express = require("express")
-const Api = require("./api")
 const app = express()
 const PORT = 8080
 
-const handlebars = require("express-handlebars")
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static(__dirname + '/public'))
+app.listen(PORT, () => console.log(`server listening on port ${PORT}`))
+
 const path = require("path")
 const viewsFolder = path.join(__dirname, "views")
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(express.static(__dirname + "/public"))
-
-//inicializar el motor de plantillas
-app.engine("handlebars", handlebars.engine())
 //las views en mi proyecto
 app.set("views", viewsFolder)
 //motor de plantillas voy a usar
-app.set("view engine", "handlebars")
+app.set("view engine", "pug")
 
-app.listen(PORT, () => console.log(`server listening on port ${PORT}`))
-
+const Api = require("./api")
 const productosApi = new Api()
 
 //....Endpoints
 app.get("/", (req, res) => {
-	res.render("formulario")
+	res.render('formulario')
 })
 
 app.post("/productos", (req, res) => {
 	let producto = req.body
 	productosApi.save(producto)
-	res.redirect("/")
+	res.redirect('/')
 })
 
 app.get("/productos", (req, res) => {
 	const productos = productosApi.getAll()
-	res.render("productos", { products: productos })
+	res.render('productos', {products: productos})
 })
+
